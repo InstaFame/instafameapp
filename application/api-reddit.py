ad1379c1a390b7be5afab219c6417fbbbc21ac33
instafame <instafameapp@gmail.com>
2018-02-15 21:20:01 +0000

    Github Auto-commit via Instafame Cron 2018-02-15_21:20:01

--- application/api-reddit.py
+++ application/api-reddit.py
@@ -1,5 +1,9 @@
-import requests
+#!/usr/bin/python
+import praw
 
-r = request.get(‘http://www.reddit.com/user/spilcm/comments/.json’)
+reddit = praw.Reddit(client_id='LOeda70Z33ctEA',
+    client_secret='C3W-YID8rbZaDhlfIJQPgIbKcys',
+    redirect_uri='https://github.com/InstaFame/instafameapp',
+    user_agent='testscript by /u/fakebot3')
 
-data = r.json()
+print(reddit.auth.scopes())
