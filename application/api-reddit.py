3c8b8c27f8bccbc3b85d219a4e1a85ea67134fc8
instafame <instafameapp@gmail.com>
2018-02-15 21:40:01 +0000

    Github Auto-commit via Instafame Cron 2018-02-15_21:40:01

--- application/api-reddit.py
+++ application/api-reddit.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
 import praw
+import os
 
-reddit = praw.Reddit(client_id='LOeda70Z33ctEA',
-    client_secret='C3W-YID8rbZaDhlfIJQPgIbKcys',
-    redirect_uri='https://github.com/InstaFame/instafameapp',
-    user_agent='testscript by /u/fakebot3')
+# Setup Authentication Credentials
+def init():
+    global reddit
+    reddit = praw.Reddit(client_id='LOeda70Z33ctEA',
+        client_secret='C3W-YID8rbZaDhlfIJQPgIbKcys',
+        redirect_uri='https://github.com/InstaFame/instafameapp',
+        user_agent='testscript by /u/instfameapp')
 
-print(reddit.auth.scopes())
+    # Enable read only mode
+    reddit.read_only = True
+
+def buildPostsList():
+    if not os.path.isfile("posts_replied_to.txt"):
+        posts_replied_to = []
+    else:
+        with open("posts_replied_to.txt", "r") as f:
+           posts_replied_to = f.read()
+           posts_replied_to = posts_replied_to.split("\n")
+           posts_replied_to = list(filter(None, posts_replied_to))
+
+    return posts_replied_to
+
+
+
+def getTopPosts(subInput):
+    posts_replied_to = buildPostsList()
+
+    subreddit = reddit.subreddit(subInput)
+
+    for submission in subreddit.hot(limit=5):
+        if submission.id not in posts_replied_to :
+            print(submission.mod.sticky())
+
+init()
+getTopPosts('aww')
