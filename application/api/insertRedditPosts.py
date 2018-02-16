119af99712b7839beaebeeb4aa9398a33b138d55
instafame <instafameapp@gmail.com>
2018-02-16 18:40:01 +0000

    Github Auto-commit via Instafame Cron 2018-02-16_18:40:01

--- application/api/insertRedditPosts.py
+++ application/api/insertRedditPosts.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import getRedditPosts
+import sendmail
 import psycopg2
 
 def connectToDB():
@@ -23,15 +24,31 @@ def insertStaging(subreddit):
 
     dict_data = getRedditPosts.main(subreddit)
 
+    failed_rows = 0
+    successful_rows = 0
+    insertError = ''
+
     # Loop through list for individual dictionaries
     for i in dict_data:
         # Insert each dict as a row
         sql = "INSERT INTO stg." + str(subreddit) + " (" + ", ".join(i.keys()) + ") VALUES (" + ", ".join(["%("+k+")s" for k in i]) + ");"
-        execute = cur.execute(sql, i)
-        commit = conn.commit()
-        print commit
+        try:
+            execute = cur.execute(sql, i)
+            commit = conn.commit()
+            successful_rows = successful_rows + 1
+        except Exception as e:
+            insertError = e
+            failed_rows = failed_rows + 1
 
-
+    if failed_rows == 0:
+        sendmail.sendAlert("Insert Staging Subreddit: /r/" + str(subreddit) + " - SUCCESS",
+    "Job inserting staging was successful\n\nSuccessfully Inserted Rows: " + str(successful_rows) + "\nFailed Inserted Rows: " + str(failed_rows))
+    elif failed_rows != 0 and successful_rows != 0:
+        sendmail.sendAlert("Insert Staging Subreddit: /r/" + str(subreddit) + " - PARTIAL",
+    "Job inserting staging was successfuly with errors.\n\nSuccessfully Inserted Rows: " + str(successful_rows) + "\nFailed Inserted Rows: " + str(failed_rows))
+    else:
+        sendmail.sendAlert("Insert Staging Subreddit: /r/" + str(subreddit) + " - FAILED",
+    "Job inserting staging has failed with all inserts.\n\nSuccessfully Inserted Rows: " + str(successful_rows) + "\nFailed Inserted Rows: " + str(failed_rows) + "\n\nError message: " + str(insertError))
 
 if __name__ == '__main__':
     connectToDB()
--- application/api/sendmail.py
+++ application/api/sendmail.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("YOUR EMAIL ADDRESS", "YOUR PASSWORD")
+def sendAlert(subject, emailBody):
+    fromaddr = "instafame.alerts@gmail.com"
+    toaddr = "instafameapp@gmail.com"
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = str(subject)
 
-msg = "YOUR MESSAGE!"
-server.sendmail("YOUR EMAIL ADDRESS", "THE EMAIL ADDRESS TO SEND TO", msg)
-server.quit()
+    body = str(emailBody)
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "Qus6TzHQbFd454UTeLqSQQwWNP3QRW8DNtjTjJ2SEhw4EPYTNHTPKMYDaQcFkGwBF6w8xtacTnbUsJke7n5nyhjYfCauaBXs3gu7")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
