2d83ad8b1e921248f4c2ab279b7b9d28a2c60dbb
instafame <instafameapp@gmail.com>
2018-02-16 18:00:02 +0000

    Github Auto-commit via Instafame Cron 2018-02-16_18:00:02

--- application/api/insertRedditPosts.py
+++ application/api/insertRedditPosts.py
@@ -3,11 +3,35 @@
 import getRedditPosts
 import psycopg2
 
+def connectToDB():
+    global cur
+    # Connect to database
+    conn = psycopg2.connect(dbname="instafame", user="instafame", password="", port=5432)
+
+    # Create connection cursor
+    cur = conn.cursor()
+
+def insertStaging(subreddit):
+
+    try:
+        cur.execute("SELECT version()")
+        rows = cur.fetchall()
+        print rows
+    except psycopg2.Error as e:
+        print "Could not fetch database version. Error: " + str(e.pgerror)
+
+    dict_data = getRedditPosts.main(subreddit)
+
+    # Loop through list for individual dictionaries
+    for i in dict_data:
+        # Insert each dict as a row
+        ql = "INSERT INTO stg." + str(subreddit) + " (" + ", ".join(i.keys()) + ") VALUES (" + ", ".join(["%("+k+")s" for k in i]) + ");"
+        cur.execute(sql, dict_data)
+
+
 
 
 
 if __name__ == '__main__':
-    dict_data = getRedditPosts.main('aww')
-    for i in dict_data:
-        ql = "INSERT INTO table (" + ", ".join(i.keys()) + ") VALUES (" + ", ".join(["%("+k+")s" for k in i]) + ");"
-        print ql
+    connectToDB()
+    insertStaging('aww')
