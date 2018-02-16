0d0c131ad60fbc5054ccdbf6cc837de9569a4659
instafame <instafameapp@gmail.com>
2018-02-16 16:40:01 +0000

    Github Auto-commit via Instafame Cron 2018-02-16_16:40:01

--- application/api-reddit.py
+++ application/api-reddit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import praw
 import os
+from urlparse import urlparse
 
 # Setup Authentication Credentials
 def init():
@@ -28,6 +29,24 @@ def buildPostsList():
 
     return posts_replied_to
 
+def parseUrl(submissionLink):
+    parsedUrl = urlparse(submissionLink)
+    domain = '{uri.netloc}'.format(uri=parsedUrl)
+
+    return domain
+
+def setDomain(domain):
+    if domain == 'i.imgur.com':
+        domain = 'imgur'
+    elif domain == 'gfycat.com':
+        domain = 'gfycat'
+    elif domain == 'i.redd.it':
+        domain = 'reddit'
+    else:
+        domain = 'unparsed'
+
+    return domain
+
 # Function to get the top posts
 def getTopPosts(subInput):
     # Get list of already posted posts
@@ -38,22 +57,30 @@ def getTopPosts(subInput):
 
     # Loop through the top 5 hottest current posts
     for submission in subreddit.top('month'):
-
+        looper = 0
         # Check if post has already been made, also make sure you're not looking at a stickied thread
         if submission.id not in posts_replied_to and submission.stickied == False :
             print("Title: " + submission.title)
-            print("Link ID: " + submission.id)
+            print("Link ID: " + str(submission.id))
             print("Is Reddit Media Domain: " + str(submission.is_reddit_media_domain))
             print("Is Video: " + str(submission.is_video))
-            print("Media: " + str(submission.media)
-            print("Media Embed: " + str(submission.media_embed)
-            print("Score: " + str(submission.score)
+            print("Media: " + str(submission.media))
+            print("Media Embed: " + str(submission.media_embed))
+            print("Score: " + str(submission.score))
             print("Shortlink: " + str(submission.shortlink))
-            print("Is Spoiler: " + str(submission.spoiler)
-            print("Thumbnail Height: " + str(submission.thumbnail_height)
-            print("Thumbnail Width: " + str(submission.thumbnail_width)
-            print("View Count: " + submission.view_count)
+            print("Is Spoiler: " + str(submission.spoiler))
+            print("Thumbnail Height: " + str(submission.thumbnail_height))
+            print("Thumbnail Width: " + str(submission.thumbnail_width))
+            print("View Count: " + str(submission.view_count))
+            print("URL: " + str(submission.url))
+            print("URL Parent: " + str(parseUrl(submission.url)))
+            print("Domain: ") + str(setDomain(parseUrl(submission.url)))
             print('############################')
+
+            if looper > 3:
+                break
+            else:
+                looper = looper + 11
 
 
 init()
